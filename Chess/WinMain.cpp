4bb886eeeb7ff96d8ab966dc583c2dddbbcac4ff
RadAd <adamgates84+github@gmail.com>
2025-07-16 16:29:35 +1000

    Upgrade to latest WinMain

--- Chess/WinMain.cpp
+++ Chess/WinMain.cpp
@@ -1,17 +1,19 @@
-#define STRICT
+#define STRICT 1
 //#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <tchar.h>
+#include <exception>
+#include <system_error>
 //#include <objbase.h>
 #include <commctrl.h>
 #include <gdiplus.h>
+
+//#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 HINSTANCE g_hInstance = NULL;
 HACCEL g_hAccelTable = NULL;
 HWND g_hWndAccel = NULL;
 HWND g_hWndDlg = NULL;
-
-//#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 bool Run(_In_ const LPCTSTR lpCmdLine, _In_ const int nShowCmd);
 
@@ -30,9 +32,59 @@ int DoMessageLoop()
     return int(msg.wParam);
 }
 
-int WINAPI _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nShowCmd)
+void DisplayError(const std::exception& e, const char* title)
 {
-    int ret = 0;
+    MessageBoxA(NULL, e.what(), title, MB_ICONERROR | MB_OK);
+}
+
+#if _DEBUG
+void __cdecl CrtDebugEnd()
+{
+    _CrtMemState state;
+    _CrtMemCheckpoint(&state);
+
+    if (state.lCounts[_CLIENT_BLOCK] != 0 ||
+        state.lCounts[_NORMAL_BLOCK] != 0 ||
+        (_crtDbgFlag & _CRTDBG_CHECK_CRT_DF && state.lCounts[_CRT_BLOCK] != 0))
+    {
+        _RPT0(_CRT_ERROR, "Detected memory leaks!\n");
+    }
+}
+
+void CrtDebugInitFile(LPCTSTR lpDebugFileName)
+{
+    const HANDLE hLogFile = CreateFile(lpDebugFileName, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    _ASSERTE(hLogFile);
+    if (hLogFile && hLogFile != INVALID_HANDLE_VALUE)
+    {
+        for (int e = _CRT_WARN; e < _CRT_ERRCNT; ++e)
+        {
+            _CrtSetReportMode(e, _CrtSetReportMode(e, _CRTDBG_REPORT_MODE) | _CRTDBG_MODE_FILE);
+            _CrtSetReportFile(e, hLogFile);
+        }
+    }
+}
+
+void CrtDebugInit()
+{
+    TCHAR Filename[MAX_PATH];
+    const DWORD len = GetModuleFileName(NULL, Filename, ARRAYSIZE(Filename));
+    _ASSERTE(_tcscmp(Filename + len - 4, TEXT(".exe")) == 0);
+    _tcscpy_s(Filename + len - 4, ARRAYSIZE(Filename) - len + 4, TEXT("DbgLog.txt"));
+    CrtDebugInitFile(Filename);
+    atexit(CrtDebugEnd);
+}
+#else
+#define CrtDebugInit()                   ((int)0)
+#endif
+
+int WINAPI _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nShowCmd)
+try
+{
+    _CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
+    CrtDebugInit();
+
+    int ret = EXIT_FAILURE;
     g_hInstance = hInstance;
 #ifdef _OBJBASE_H_  // from objbase.h
     if (SUCCEEDED(CoInitialize(nullptr)))
@@ -55,5 +107,20 @@ int WINAPI _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
         CoUninitialize();
 #endif
     }
+
     return ret;
+}
+catch (const std::system_error& e)
+{
+    char Filename[MAX_PATH];
+    GetModuleFileNameA(NULL, Filename, ARRAYSIZE(Filename));
+    DisplayError(e, Filename);
+    return e.code().value();
+}
+catch (const std::exception& e)
+{
+    char Filename[MAX_PATH];
+    GetModuleFileNameA(NULL, Filename, ARRAYSIZE(Filename));
+    DisplayError(e, Filename);
+    return EXIT_FAILURE;
 }
